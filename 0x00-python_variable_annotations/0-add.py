ec3df3e1dde2a6082b01cc1d7e1f9c987d5f3059
Hassan Cissey <hcissey0@gmail.com>
2024-01-11 23:07:05 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This is just an add function"""
+
+
+def add(a: float, b: float) -> float:
+    """This function is a sum function
+
+    Args:
+        a (float): first operand
+        b (float): second operand
+
+    Returns:
+        float: The sum of both the operands
+    """
+    return a + b
