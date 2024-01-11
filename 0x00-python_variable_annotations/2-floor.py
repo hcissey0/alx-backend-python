ec33d7da769b82c646ac4b32b10ac6f405add2ce
Hassan Cissey <hcissey0@gmail.com>
2024-01-11 23:11:46 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This floors a float to integer"""
+
+
+def floor(n: float) -> int:
+    """Finds the floor of a float
+
+    Args:
+        n (float): The float
+
+    Returns:
+        int: The floor of the float
+    """
+    import math
+    return math.floor(n)
