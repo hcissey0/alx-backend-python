86760c173903588bc88af919c7a3cb1090de2dcb
Hassan Cissey <hcissey0@gmail.com>
2024-01-11 23:38:47 +0000

    Complex types - functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This is going to make a multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable:
+    """This is going to make a multiplier
+
+    Args:
+        multiplier (float): The multiplier
+
+    Returns:
+        Callable: The return function
+    """
+    return lambda x: x * x
