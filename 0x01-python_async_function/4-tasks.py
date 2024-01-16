2df33d26c2a04ca8363d497912e735b05c853287
Hassan Cissey <hcissey0@gmail.com>
2024-01-16 05:54:57 +0000

    task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""thisbfile is also hete to create a task"""
+import asyncio
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n, max_delay):
+    tasks = []
+    for i in range(n):
+        tasks.append(task_wait_random(max_delay))
+    delays = []
+    while tasks:
+        done, tasks = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+        for task in done:
+            delays.append(task.result())
+    return delays
+
