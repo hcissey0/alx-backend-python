f84b05c86c09aa804994762dfea93197d2473a2f
Hassan Cissey <hcissey0@gmail.com>
2024-01-15 20:48:00 -0800

    Concurrency and coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,14 +2,17 @@
 """this is the concurrent coroutines file"""
 import asyncio
 import random
+from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax.py').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """this is the wait function in pthon"""
-    delays = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    re = await asyncion.gather(*delays, return_exceptions=True)
-    delays[:] = [r[0] for r in res]
+    ts = [wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for t in asyncio.as_completed(ts):
+        delay = await t
+        delays.append(delay)
     return delays
