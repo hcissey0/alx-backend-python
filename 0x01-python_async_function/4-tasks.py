b64ed11b78aebe9686cbbd206105d810ae829212
Hassan Cissey <hcissey0@gmail.com>
2024-01-16 06:00:40 +0000

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """thisbfile is also hete to create a task"""
 import asyncio
+from typing import List
 
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """this fubction is similar to wait_n"""
     tasks = []
     for i in range(n):
